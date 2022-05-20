62c949194d1b448e90595f789abbf4c9ff889c51
sss654654 <zed6740@naver.com>
2022-05-20 10:09:24 +0900

    QR, QRscanner구현 완료 qr은 작동하나 qrscanner는 아직 테스트 안함

--- src/screens/QRScanner.js
+++ src/screens/QRScanner.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import QRrequest from '../funtion/QRrequest';
+import request from '../funtion/request';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const QRScanner = ({navigation}) => {
+
+const QRScanner = ({route}) => {
+  const meetingId = route.params.id;
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
+  const storeQRtoken = async (value) => {
+    try {
+      await AsyncStorage.setItem("qrToken", value)
+    } catch (e) {
+      // saving error
+    }
+  }
   useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
@@ -13,9 +25,33 @@ const QRScanner = ({navigation}) => {
     })();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
+  const handleBarCodeScanned = async({ type, token }) => {
     setScanned(true);
-    alert(`바코드의 형태 : ${type} 데이터 값 :  ${data}`);
+    alert(`바코드의 형태 : ${type} 데이터 값 :  ${token}`);
+
+    storeQRtoken(token);
+    const res = await QRrequest({
+      method: 'POST',
+      url: `/qr/create`,
+    });
+
+    if(res.result === "SUCCESS"){
+      const response = await request({
+        method: 'POST',
+        url: `/${res.data}/meeting/get/${meetingId}`,
+      });
+  
+      if(response.result === "SUCCESS"){
+        setMeeting(response.data);
+      }
+      res.data;
+      // console.log("res : ",res);
+      // console.log("res.data : ",res.data);
+      // console.log("value : ",value);
+    }
+    else{
+      alert(res.data);
+    }
   };
 
   if (hasPermission === null) {
